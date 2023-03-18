386568a6db6c532565968394c0cdd83fbb2fcaab
Ak-Dh <akhilldj@gmail.com>
2023-03-18 10:34:28 +0100

    Phase2 04

--- javaScript/profile.js
+++ javaScript/profile.js
@@ -1,14 +1,13 @@
 function includeNavbar() {
   const navbarElement = document.getElementById("navbar");
   if (navbarElement) {
-    const navbarXhr = new XMLHttpRequest();
-    navbarXhr.open("GET", "../pages/navbar.html", true);
-    navbarXhr.send();
-
-    window.addEventListener("load", function() {
-      if (navbarXhr.readyState == 4 && navbarXhr.status == 200) {
-        navbarElement.innerHTML = navbarXhr.responseText;
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        navbarElement.innerHTML = this.responseText;
       }
-    });
+    };
+    xhr.open("GET", "../pages/navbar.html", true);
+    xhr.send();
   }
 }
