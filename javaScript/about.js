126969fa568e283be5152d1b98e783019acf3578
Ak-Dh <akhilldj@gmail.com>
2023-03-18 10:22:42 +0100

    Phase2 02

--- javaScript/about.js
+++ javaScript/about.js
@@ -1,10 +1,14 @@
 function includeNavbar() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("navbar").innerHTML = this.responseText;
+  const navbarElement = document.getElementById("navbar");
+  if (navbarElement) {
+    const navbarXhr = new XMLHttpRequest();
+    navbarXhr.open("GET", "navbar.html", true);
+    navbarXhr.send();
+
+    window.addEventListener("load", function() {
+      if (navbarXhr.readyState == 4 && navbarXhr.status == 200) {
+        navbarElement.innerHTML = navbarXhr.responseText;
       }
-    };
-    xhttp.open("GET", "../pages/navbar.html", true);
-    xhttp.send();
+    });
   }
+}
