a04fb5b1e2e4844a2f93371e724db8881d46905e
Ak-Dh <akhilldj@gmail.com>
2023-04-18 10:45:24 +0200

    new changes

--- javaScript/settings.js
+++ javaScript/settings.js
@@ -11,3 +11,26 @@ function includeNavbar() {
     xhr.send();
   }
 }
+
+const themeToggle = document.getElementById('toggle-switch');
+const root = document.documentElement;
+
+themeToggle.addEventListener('change', function() {
+  if (this.checked) {
+    root.style.setProperty('--blue', 'rgb(0, 100, 157)');
+    root.style.setProperty('--text-field-light-grey', 'rgb(241, 235, 235)');
+    root.style.setProperty('--nav-selected', '#ffffffe4');
+    root.style.setProperty('--nav-hover', 'rgb(2, 120, 188)');
+    root.style.setProperty('--black', 'rgb(0, 0, 0)');
+    root.style.setProperty('--search-input-field', '#f1f1f1');
+    root.style.setProperty('--grey', '#808080');
+  } else {
+    root.style.setProperty('--blue', 'rgb(89, 170, 214)');
+    root.style.setProperty('--text-field-light-grey', 'rgb(241, 235, 235)');
+    root.style.setProperty('--nav-selected', '#ffffffe4');
+    root.style.setProperty('--nav-hover', 'rgb(116, 193, 235)');
+    root.style.setProperty('--black', 'rgb(0, 0, 0)');
+    root.style.setProperty('--search-input-field', '#f1f1f1');
+    root.style.setProperty('--grey', '#808080');
+  }
+});
